89b359f81d8ff7da0944dc054e858c89a1e6b187
KeithYue <buaawangyue@gmail.com>
2013-09-12 11:14:02 +0800

    add js folder

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.write('Hello ');
+// });
+// $(document).ready(function(){
+//     $(document).write('Hello World');
+//     });
+// 
