0ed74e82c7fc707b04ba70821fd17692ff73e373
KeithYue <buaawangyue@gmail.com>
2013-09-13 18:16:59 +0800

    make the extension to load css images using web_accessible_resources. Now can design my own extension UI

---
+++ js/background.js
@@ -0,0 +1,7 @@
+//turn the page to red
+chrome.browserAction.onClicked.addListener(function(tab){
+    alert(chrome.extension.getURL('css/'));
+    chrome.tabs.executeScript(null, {
+        file: "js/main.js"
+        });
+    });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.write('Hello ');
-// });
-// $(document).ready(function(){
-//     $(document).write('Hello World');
-//     });
-// 
+//init the video head track
+console.log('I am the main js');
+
+// init the dialog html strings
+var trackDialog = $('<div id="track-dialog" title="Rock Your Page">Wang Yue</div>');
+var inputCanvas = $('<canvas id="inputCanvas" width="320" height="240" style="display:none"></canvas>');
+var inputVideo = $('<video id="inputVideo" autoplay loop></video>');
+
+$('body').append(trackDialog);
+trackDialog.append(inputCanvas);
+trackDialog.append(inputVideo);
+
+$('#track-dialog').dialog({
+    autoOpen: false,
+    resizable: false,
+    position:{
+        my: "right top",
+        at: "right top",
+        of: $(window)
+        },
+    show: {
+        effect: "blind",
+        duration: 1000
+        },
+    hide: {
+        effect: "explode",
+        duration: 1000
+        }
+    });
+
+//open the dialog in the right, top of the window
+$('#track-dialog').parent().css({position:"fixed"}).end().dialog('open');
+
+//begin video tracking
+var videoInput = document.getElementById('inputVideo');
+var canvasInput = document.getElementById('inputCanvas');
+var htracker = new headtrackr.Tracker();
+htracker.init(videoInput, canvasInput);
+htracker.start();
