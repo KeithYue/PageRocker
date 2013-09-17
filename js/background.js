64790e23f828a9e734c134bba2de0dc9ce446ccf
KeithYue <buaawangyue@gmail.com>
2013-09-17 14:31:21 +0800

    mv the html pages to sub-directory

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 //turn the page to red
 chrome.browserAction.onClicked.addListener(function(tab){
     // alert(chrome.extension.getURL('css/'));
+    // chrome.tabs.executeScript(null, {
+    //     file: "js/main.js"
+    //     });
     chrome.tabs.executeScript(null, {
-        file: "js/main.js"
+        file: "js/css3d.js"
         });
     });
---
+++ js/css3d.js
@@ -0,0 +1,1 @@
+console.log('this is css 3d script');
